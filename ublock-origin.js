d19c986343d5751dc525585e66ae36fd8a592719
Xatoto <127494285+Xatoto@users.noreply.github.com>
2023-11-18 14:51:56 +0100

    Update ublock-origin.js

--- ublock-origin.js
+++ ublock-origin.js
@@ -1,6 +1,18 @@
-twitch-videoad.js text/javascript
+// ==UserScript==
+// @name         TwitchAdSolutions (vaft)
+// @namespace    https://github.com/pixeltris/TwitchAdSolutions
+// @version      5.8.5
+// @description  Multiple solutions for blocking Twitch ads (vaft)
+// @updateURL    https://github.com/pixeltris/TwitchAdSolutions/raw/master/vaft/vaft.user.js
+// @downloadURL  https://github.com/pixeltris/TwitchAdSolutions/raw/master/vaft/vaft.user.js
+// @author       https://github.com/cleanlock/VideoAdBlockForTwitch#credits
+// @match        *://*.twitch.tv/*
+// @run-at       document-start
+// @inject-into  page
+// @grant        none
+// ==/UserScript==
 (function() {
-    if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
+    'use strict';
     //This stops Twitch from pausing the player when in another tab and an ad shows.
     try {
         Object.defineProperty(document, 'visibilityState', {
@@ -57,10 +69,8 @@ twitch-videoad.js text/javascript
         scope.ClientID = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
         scope.ClientVersion = 'null';
         scope.ClientSession = 'null';
-        //scope.PlayerType1 = 'site'; //Source - NOTE: This is unused as it's implicitly used by the website iself
-        scope.PlayerType2 = 'autoplay'; //1080p
-        scope.PlayerType3 = 'embed'; //Source
-        //scope.PlayerType4 = 'embed'; //Source
+        scope.PlayerType2 = 'embed'; //Source
+        scope.PlayerType3 = 'autoplay'; //360p
         scope.CurrentChannelName = null;
         scope.UsherParams = null;
         scope.WasShowingAd = false;
@@ -284,9 +294,6 @@ twitch-videoad.js text/javascript
                             if (weaverText.includes(AdSignifier)) {
                                 weaverText = await processM3U8(url, responseText, realFetch, PlayerType3);
                             }
-                            //if (weaverText.includes(AdSignifier)) {
-                            //    weaverText = await processM3U8(url, responseText, realFetch, PlayerType4);
-                            //}
                             resolve(new Response(weaverText));
                         };
                         var send = function() {
@@ -316,6 +323,7 @@ twitch-videoad.js text/javascript
                                     StreamInfos[channelName] = streamInfo = {};
                                 }
                                 streamInfo.ChannelName = channelName;
+                                streamInfo.RequestedAds = new Set();
                                 streamInfo.Urls = [];// xxx.m3u8 -> { Resolution: "284x160", FrameRate: 30.0 }
                                 streamInfo.EncodingsM3U8Cache = [];
                                 streamInfo.EncodingsM3U8 = encodingsM3u8;
@@ -476,6 +484,21 @@ twitch-videoad.js text/javascript
             var isMidroll = textStr.includes('"MIDROLL"') || textStr.includes('"midroll"');
             //Reduces ad frequency. TODO: Reduce the number of requests. This is really spamming Twitch with requests.
             if (!isMidroll) {
+                if (playerType === PlayerType2) {
+                    var lines = textStr.replace('\r', '').split('\n');
+                    for (var i = 0; i < lines.length; i++) {
+                        var line = lines[i];
+                        if (line.startsWith('#EXTINF') && lines.length > i + 1) {
+                            if (!line.includes(',live') && !streamInfo.RequestedAds.has(lines[i + 1])) {
+                                // Only request one .ts file per .m3u8 request to avoid making too many requests
+                                //console.log('Fetch ad .ts file');
+                                streamInfo.RequestedAds.add(lines[i + 1]);
+                                fetch(lines[i + 1]).then((response)=>{response.blob()});
+                                break;
+                            }
+                        }
+                    }
+                }
                 try {
                     //tryNotifyTwitch(textStr);
                 } catch (err) {}
@@ -663,7 +686,7 @@ twitch-videoad.js text/javascript
     }
     function gqlRequest(body, realFetch) {
         if (ClientIntegrityHeader == null) {
-            console.warn('ClientIntegrityHeader is null');
+            //console.warn('ClientIntegrityHeader is null');
             //throw 'ClientIntegrityHeader is null';
         }
         var fetchFunc = realFetch ? realFetch : fetch;
@@ -861,16 +884,20 @@ twitch-videoad.js text/javascript
                         }
                         //Client integrity header
                         ClientIntegrityHeader = init.headers['Client-Integrity'];
-                        twitchMainWorker.postMessage({
-                            key: 'UpdateClientIntegrityHeader',
-                            value: init.headers['Client-Integrity']
-                        });
+                        if (ClientIntegrityHeader && twitchMainWorker) {
+                            twitchMainWorker.postMessage({
+                                key: 'UpdateClientIntegrityHeader',
+                                value: init.headers['Client-Integrity']
+                            });
+                        }
                         //Authorization header
                         AuthorizationHeader = init.headers['Authorization'];
-                        twitchMainWorker.postMessage({
-                            key: 'UpdateAuthorizationHeader',
-                            value: init.headers['Authorization']
-                        });
+                        if (AuthorizationHeader && twitchMainWorker) {
+                            twitchMainWorker.postMessage({
+                                key: 'UpdateAuthorizationHeader',
+                                value: init.headers['Authorization']
+                            });
+                        }
                     }
                     //To prevent pause/resume loop for mid-rolls.
                     if (url.includes('gql') && init && typeof init.body === 'string' && init.body.includes('PlaybackAccessToken') && init.body.includes('picture-by-picture')) {
